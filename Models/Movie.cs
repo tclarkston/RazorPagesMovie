6bfd13d9e121792a92cbe533e23c7241b5ce50c9
tclarkston <37628195+tclarkston@users.noreply.github.com>
2024-02-19 10:56:08 -0700

    first commit

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models;
+
+public class Movie
+{
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    [DataType(DataType.Date)]
+    [Display(Name = "Release Date")]
+    public DateTime ReleaseDate { get; set; }
+    public string? Genre { get; set; }
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Price { get; set; }
+}
